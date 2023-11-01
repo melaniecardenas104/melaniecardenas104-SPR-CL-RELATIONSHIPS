60734cc3e0d1c468d88169af951e69b05c0049b2
Melanie Cardenas <melaniecardenas19@yahoo.com>
2023-11-01 18:42:18 +0000

    C124 Annotations that link entities @ManyToOne (artist), @OneToMany (songs)

--- src/main/java/Lab/Model/Album.java
+++ src/main/java/Lab/Model/Album.java
@@ -30,11 +30,13 @@ public class Album {
     /**
      * Review the other model classes to see examples of annotations that link entities.
      */
+    @ManyToOne
     private Artist artist;
 
     /**
      * Review the other model classes to see examples of annotations that link entities.
      */
+    @OneToMany
     private List<Song> songs;
 
     public Album(String title) {
